8c0ee279f8c150ce8bc88afed69eaab30cf38139
Manoj <manoj@peakperformer.io>
2023-01-02 09:03:42 +0530

    version folder

--- 1/model.py
+++ 1/model.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -25,148 +25,117 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import json
-import numpy as np
-import torch
-import triton_python_backend_utils as pb_utils
 
-from torch import autocast
-from torch.utils.dlpack import to_dlpack, from_dlpack
-from transformers import CLIPTokenizer
-from diffusers import LMSDiscreteScheduler, UNet2DConditionModel
-from tqdm.auto import tqdm
+# triton_python_backend_utils is available in every Triton Python model. You
+# need to use this module to create inference requests and responses. It also
+# contains some utility functions for extracting information from model_config
+# and converting Triton input/output types to numpy types.
+import triton_python_backend_utils as pb_utils
 
 
 class TritonPythonModel:
+    """Your Python model must use the same class name. Every Python model
+    that is created must have "TritonPythonModel" as the class name.
+    """
+
     def initialize(self, args):
-        self.output_dtype = pb_utils.triton_string_to_numpy(
-            pb_utils.get_output_config_by_name(
-                json.loads(args["model_config"]), "generated_image"
-            )["data_type"]
+        """`initialize` is called only once when the model is being loaded.
+        Implementing `initialize` function is optional. This function allows
+        the model to intialize any state associated with this model.
+
+        Parameters
+        ----------
+        args : dict
+          Both keys and values are strings. The dictionary keys and values are:
+          * model_config: A JSON string containing the model configuration
+          * model_instance_kind: A string containing model instance kind
+          * model_instance_device_id: A string containing model instance device ID
+          * model_repository: Model repository path
+          * model_version: Model version
+          * model_name: Model name
+        """
+
+        # You must parse model_config. JSON string is not parsed here
+        self.model_config = model_config = json.loads(args["model_config"])
+
+        # Get OUTPUT0 configuration
+        output0_config = pb_utils.get_output_config_by_name(model_config, "OUTPUT0")
+
+        # Get OUTPUT1 configuration
+        output1_config = pb_utils.get_output_config_by_name(model_config, "OUTPUT1")
+
+        # Convert Triton types to numpy types
+        self.output0_dtype = pb_utils.triton_string_to_numpy(
+            output0_config["data_type"]
         )
-        self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
-        self.scheduler = LMSDiscreteScheduler(
-            beta_start=0.00085,
-            beta_end=0.012,
-            beta_schedule="scaled_linear",
-            num_train_timesteps=1000,
+        self.output1_dtype = pb_utils.triton_string_to_numpy(
+            output1_config["data_type"]
         )
-        self.unet = UNet2DConditionModel.from_pretrained(
-            "##model_name##",
-            subfolder="unet",
-            revision="fp16",
-            torch_dtype=torch.float16,
-        ).to("cuda")
 
     def execute(self, requests):
+        """`execute` MUST be implemented in every Python model. `execute`
+        function receives a list of pb_utils.InferenceRequest as the only
+        argument. This function is called when an inference request is made
+        for this model. Depending on the batching configuration (e.g. Dynamic
+        Batching) used, `requests` may contain multiple requests. Every
+        Python model, must create one pb_utils.InferenceResponse for every
+        pb_utils.InferenceRequest in `requests`. If there is an error, you can
+        set the error argument when creating a pb_utils.InferenceResponse
+
+        Parameters
+        ----------
+        requests : list
+          A list of pb_utils.InferenceRequest
+
+        Returns
+        -------
+        list
+          A list of pb_utils.InferenceResponse. The length of this list must
+          be the same as `requests`
+        """
+
+        output0_dtype = self.output0_dtype
+        output1_dtype = self.output1_dtype
+
         responses = []
+
+        # Every Python backend must iterate over everyone of the requests
+        # and create a pb_utils.InferenceResponse for each of them.
         for request in requests:
-            inp = pb_utils.get_input_tensor_by_name(request, "prompt")
-            input_text = inp.as_numpy()[0][0].decode()
+            # Get INPUT0
+            in_0 = pb_utils.get_input_tensor_by_name(request, "INPUT0")
+            # Get INPUT1
+            in_1 = pb_utils.get_input_tensor_by_name(request, "INPUT1")
 
-            # tokenizing
-            tokenized_text = self.tokenizer(
-                [input_text],
-                padding="max_length",
-                max_length=self.tokenizer.model_max_length,
-                truncation=True,
-                return_tensors="pt",
-            ).input_ids
-            tokenized_text_uncond = self.tokenizer(
-                [""],
-                padding="max_length",
-                max_length=self.tokenizer.model_max_length,
-                truncation=True,
-                return_tensors="pt",
-            ).input_ids
-
-            # Querying the text_encoding model
-            input_ids_1 = pb_utils.Tensor(
-                "input_ids",
-                np.concatenate(
-                    [
-                        tokenized_text_uncond.numpy().astype(np.int32),
-                        tokenized_text.numpy().astype(np.int32),
-                    ]
-                ),
-            )
-            encoding_request = pb_utils.InferenceRequest(
-                model_name="text_encoder",
-                requested_output_names=["last_hidden_state"],
-                inputs=[input_ids_1],
+            out_0, out_1 = (
+                in_0.as_numpy() + in_1.as_numpy(),
+                in_0.as_numpy() - in_1.as_numpy(),
             )
 
-            response = encoding_request.exec()
-            if response.has_error():
-                raise pb_utils.TritonModelException(response.error().message())
-            else:
-                text_embeddings = pb_utils.get_output_tensor_by_name(
-                    response, "last_hidden_state"
-                )
-            text_embeddings = from_dlpack(text_embeddings.to_dlpack()).clone()
-            text_embeddings = text_embeddings.to("cuda")
+            # Create output tensors. You need pb_utils.Tensor
+            # objects to create pb_utils.InferenceResponse.
+            out_tensor_0 = pb_utils.Tensor("OUTPUT0", out_0.astype(output0_dtype))
+            out_tensor_1 = pb_utils.Tensor("OUTPUT1", out_1.astype(output1_dtype))
 
-            # Running Scheduler
-            guidance_scale = 7.5
-            latents = torch.randn(
-                (text_embeddings.shape[0] // 2, self.unet.in_channels, 64, 64)
-            ).to("cuda")
-
-            self.scheduler.set_timesteps(50)
-            latents = latents * self.scheduler.sigmas[0]
-
-            for i, t in tqdm(enumerate(self.scheduler.timesteps)):
-                latent_model_input = torch.cat([latents] * 2)
-                latent_model_input = self.scheduler.scale_model_input(
-                    latent_model_input, t
-                )
-
-                with torch.no_grad(), torch.autocast("cuda"):
-                    noise_pred = self.unet(
-                        latent_model_input, t, encoder_hidden_states=text_embeddings
-                    ).sample
-
-                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-                noise_pred = noise_pred_uncond + guidance_scale * (
-                    noise_pred_text - noise_pred_uncond
-                )
-
-                latents = self.scheduler.step(
-                    noise_pred, self.scheduler.timesteps[i], latents
-                ).prev_sample
-
-            # VAE decoding
-            latents = 1 / 0.18215 * latents
-            input_latent_1 = pb_utils.Tensor.from_dlpack(
-                "latent_sample", to_dlpack(latents)
-            )
-
-            decoding_request = pb_utils.InferenceRequest(
-                model_name="vae",
-                requested_output_names=["sample"],
-                inputs=[input_latent_1],
-            )
-
-            decoding_response = decoding_request.exec()
-            if response.has_error():
-                raise pb_utils.TritonModelException(decoding_response.error().message())
-            else:
-                decoded_image = pb_utils.get_output_tensor_by_name(
-                    decoding_response, "sample"
-                )
-            decoded_image = from_dlpack(decoded_image.to_dlpack()).clone()
-
-            decoded_image = (decoded_image / 2 + 0.5).clamp(0, 1)
-            decoded_image = decoded_image.detach().cpu().permute(0, 2, 3, 1).numpy()
-            decoded_image = (decoded_image * 255).round().astype("uint8")
-
-            # Sending results
+            # Create InferenceResponse. You can set an error here in case
+            # there was a problem with handling this inference request.
+            # Below is an example of how you can set errors in inference
+            # response:
+            #
+            # pb_utils.InferenceResponse(
+            #    output_tensors=..., TritonError("An error occured"))
             inference_response = pb_utils.InferenceResponse(
-                output_tensors=[
-                    pb_utils.Tensor(
-                        "generated_image",
-                        np.array(decoded_image, dtype=self.output_dtype),
-                    )
-                ]
+                output_tensors=[out_tensor_0, out_tensor_1]
             )
             responses.append(inference_response)
+
+        # You should return a list of pb_utils.InferenceResponse. Length
+        # of this list must match the length of `requests` list.
         return responses
+
+    def finalize(self):
+        """`finalize` is called only once when the model is being unloaded.
+        Implementing `finalize` function is OPTIONAL. This function allows
+        the model to perform any necessary clean ups before exit.
+        """
+        print("Cleaning up...")
