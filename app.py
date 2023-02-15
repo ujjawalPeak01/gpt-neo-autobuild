20765187f0074621f40de8ad1c2b8cc6fb0dcd55
Manoj <manoj@peakperformer.io>
2023-02-15 11:18:43 +0530

    template method change

--- app.py
+++ app.py
@@ -1,66 +1,27 @@
-import json
-import numpy as np
-
-# The preprocess function should return input list and output list
-
-# The input list should be of the following format:
-# [{"name": <input name>, "data": <numpy array of the input data> }]
-
-# The output list should be of the following format:
-# [{"name": <name of the output>}]
-
-shape = [4]
+import torch
+import requests
+from PIL import Image
+from diffusers import StableDiffusionDepth2ImgPipeline
+from io import BytesIO
+import base64
 
 
-def preprocess(self, args):
-    print("preprocessing started....")
-    self.input0_data = np.random.rand(*shape).astype(np.float32)
-    self.input1_data = np.random.rand(*shape).astype(np.float32)
+class InferlessPythonModel:
+    def initialize(self):
+        self.pipe = StableDiffusionDepth2ImgPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-2-depth",
+            torch_dtype=torch.float16,
+        )
+        self.pipe.to("cuda")
 
-    input_list = [
-        {"name": "INPUT0", "data": self.input0_data},
-        {"name": "INPUT1", "data": self.input1_data},
-    ]
+    def infer(self, prompt, image_url):
+        url = image_url
+        init_image = Image.open(requests.get(url, stream=True).raw)
+        depth_image = self.pipe(prompt=prompt, image=init_image, strength=0.7).images[0]
+        buff = BytesIO()
+        depth_image.save(buff, format="PNG")
+        img_str = base64.b64encode(buff.getvalue())
+        return img_str
 
-    output_list = [{"name": "OUTPUT0"}, {"name": "OUTPUT1"}]
-
-    return (input_list, output_list)
-
-
-def infer(self, inputs):
-    outputs = {}
-    in_0 = inputs[0]
-    in_1 = inputs[1]
-    out_0, out_1 = (
-        in_0.as_numpy() + in_1.as_numpy(),
-        in_0.as_numpy() - in_1.as_numpy(),
-    )
-
-    outputs = [
-        {"name": "OUTPUT0", "data": out_0},
-        {"name": "OUTPUT1", "data": out_1},
-    ]
-    return outputs
-
-
-# The postprocess function recieves the requested output data as numpy arrays in the args as output dict
-# The postprocess function should return a dict of the following format:
-
-# {output: <output data>}
-
-
-def postprocess(self, args):
-
-    return_dict = {"output": "PASS: add_sub"}
-    output = args
-
-    if not np.allclose(self.input0_data + self.input1_data, output[0]):
-        print("add_sub example error: incorrect sum")
-        return_dict["output"] = "add_sub example error: incorrect sum"
-        return return_dict
-    if not np.allclose(self.input0_data - self.input1_data, output[1]):
-        print("add_sub example error: incorrect difference")
-        return_dict["output"] = "add_sub example error: incorrect difference"
-        return return_dict
-
-    return return_dict
+    def finalize(self):
+        self.pipe = None
