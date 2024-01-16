6ff0c91f110869ea06fd4943ada471ce7e3b305e
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2024-01-16 08:58:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ class InferlessPythonModel:
         prompt = inputs["prompt"]
         pipeline_output = self.generator(prompt, do_sample=True, min_length=20, max_length=300)
         generated_txt = pipeline_output[0]["generated_text"]
+        pint("ABCD")
         return {"generated_text": generated_txt}
 
     def finalize(self,args):
